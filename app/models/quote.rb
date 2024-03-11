eb5113d99ae36181433e76caa828f5b0ef47dfd9
Eleonore Baillot <eleonorebaillot@gmail.com>
2024-03-11 10:08:02 +0100

    refacto Turborails on Quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,16 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc)}
+
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # is equivalent to what's below since by default,
+    # * the target option will be equal to model_name.plural
+    # * The partial default value is equal to calling to_partial_path on an instance of the model, which by default in Rails for our Quote model is equal to "quotes/quote".
+    # * The locals default value is equal to { model_name.element.to_sym => self } which, in in the context of our Quote model, is equal to { quote: self }.
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks are equivalent to the following single line
+
+  broadcasts_to -> (quote) { "quotes" }, inserts_by: :preprend
 end
